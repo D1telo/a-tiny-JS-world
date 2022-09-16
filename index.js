10239bf191df55429d9952b53249b02e00d15c74
D1telo <bratherden@gmail.com>
2022-09-16 18:46:50 +0300

    tiny-js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { print } from './js/lib.js';
+import { print } from "./js/lib.js";
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
@@ -9,6 +9,53 @@ import { print } from './js/lib.js';
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
+const dog = {
+   species: 'dog',
+   name: 'Toby',
+   gender: 'male',
+   legs: 4,
+   hands: null,
+   voice: 'woof-woof!'
+};
+
+const cat = {
+   species: 'cat',
+   name: 'Tom',
+   gender: 'female',
+   legs: 4,
+   hands: null,
+   voice: 'meow-meow!'
+};
+
+const man = {
+   species: 'human',
+   name: 'Denys',
+   gender: 'male',
+   legs: 2,
+   hands: 2,
+   voice: 'Glory Ukraine!'
+};
+
+const woman = {
+   species: 'human',
+   name: 'Anna',
+   gender: 'female',
+   legs: 2,
+   hands: 2,
+   voice: 'Support Ukraine!'
+};
+
+const catWoman = Object.create(cat);
+catWoman.species = 'CatWoman';
+catWoman.name = 'Jesica';
+catWoman.gender = 'female';
+catWoman.legs = 2;
+catWoman.hands = 2;
+
+const speciesAll = [dog, cat, man, woman, catWoman];
+const qualityPersonage = ['species', 'name', 'gender', 'legs', 'hands', 'voice'];
+
+speciesAll.map(personage => print(qualityPersonage.map(quality => personage[quality]).join(' - ')));
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
